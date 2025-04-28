f53fe5d35b13817484dc3ef5800a8f54409ff5e7
fanfatl <gabrielfrossi1906@gmail.com>
2025-04-28 01:10:36 -0300

    switch devices

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -14,12 +14,9 @@ if (PHP_VERSION_ID < 50600) {
             echo $err;
         }
     }
-    trigger_error(
-        $err,
-        E_USER_ERROR
-    );
+    throw new RuntimeException($err);
 }
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit5cd48b6645860e6f71f44390d4549abe::getLoader();
+return ComposerAutoloaderInit4d1c2b10c00e8cf1d661023c50dd1d3a::getLoader();
